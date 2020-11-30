6b9590140bff32e2f53000092959cb0b8ac1eac1
YuliaSirius <0juli0@tut.by>
2020-11-30 16:22:52 +0300

    fix

--- js/slick.js
+++ js/slick.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   $('.theme_slider').slick({
-    infinite: true,
+    infinite: false,
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll: 3,
     dots: true,
     arrows: true,
   });
@@ -48,24 +48,98 @@ $('.mouse-parallax').on('mousemove', (e) => {
               '<div class="scroller__bar-wrapper"><div class="scroller__bar"></div></div>'
             );
         });
-        var n = {
+        let n = {
           root: o.root,
           scroller: '.scroller',
           bar: '.scroller__bar',
           barOnCls: o.barOnCls,
+          direction: 'v',
+          // direction: 'h',
         };
         baron(n);
+
+        // baron(vParams).baron(hParams);
+
         l(), l();
       });
     };
     return l();
   };
 })(jQuery);
-baron('.scroller');
+
+!(function (r) {
+  r.fn.wrapFoBaron1 = function (o) {
+    o = r.extend({ root: '.scroller_wrap', barOnCls: 'baron' }, o);
+    var l = function () {
+      r(document).ready(function () {
+        function l() {
+          r(o.root).each(function () {
+            r(this).find('.scroller__content').height() <= r(this).height()
+              ? (r(this).find('.scroller__bar-wrapper').hide(),
+                r(this).find('.scroller').addClass('.with-scroll'))
+              : r(this).find('.scroller').removeClass('.with-scroll');
+          });
+        }
+        r(o.root).each(function () {
+          r(this).wrapInner('<div class="scroller__content"></div>'),
+            r(this).wrapInner('<div class="scroller"></div>'),
+            r(this).append(
+              '<div class="scroller__bar-wrapper"><div class="scroller__bar"></div></div>'
+            );
+          // r(this).style.display = "inline-block";
+        });
+        let n = {
+          root: o.root,
+          scroller: '.scroller',
+          bar: '.scroller__bar',
+          barOnCls: o.barOnCls,
+          direction: 'h',
+        };
+        baron(n);
+
+        l(), l();
+      });
+    };
+    return l();
+  };
+})(jQuery);
 
 $(document).ready(function () {
   $(document).wrapFoBaron({
-    root: '.scroll-example',
+    root: '.scroll',
     barOnCls: 'baron',
   });
+});
+
+$(document).ready(function () {
+  $(document).wrapFoBaron({
+    root: '.scroll_audio',
+    barOnCls: 'baron',
+  });
+});
+
+$(document).ready(function () {
+  $(document).wrapFoBaron1({
+    root: '.scroll_video',
+    barOnCls: 'baron',
+  });
+});
+
+let navButtonOpen = false;
+
+let navButton = document.querySelector('.header_nav_button');
+let navigation = document.querySelector('.header_nav');
+navButton.addEventListener('click', function (e) {
+  navigation.classList.remove('navOpen');
+  navigation.classList.remove('navClose');
+
+  if (!navButtonOpen) {
+    navigation.classList.add('navOpen');
+    navButtonOpen = true;
+  } else {
+    navigation.classList.add('navClose');
+    navigation.classList.remove('navOpen');
+
+    navButtonOpen = false;
+  }
 });
